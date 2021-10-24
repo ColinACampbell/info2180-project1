1daf67ea6df8c1c9dc96248b1db579ef88f5eb67
Colin Campbell <prontobol@gmail.com>
2021-10-24 14:58:36 -0500

    Added javascript for email

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+function onFormSubmit() {
+    const messageBody = document.getElementsByClassName('message')[0];
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value
+
+    if (email.length === 0)
+        messageBody.innerHTML = `Please enter a valid email address`
+
+    else
+        messageBody.innerHTML = `Thank you! Your email address ${email} has been added to our mailing list`
+    return false;
+}
+
+
+window.onload = () => {
+    const form = document.getElementsByTagName('form')[0]
+    form.setAttribute('onsubmit', 'return onFormSubmit()')
+
+}
